0fd97bee6c7a3dd23bba31d418744dc0b8aef84f
niklomi <nikloomi@gmai.com>
2015-12-13 09:43:23 -0800

    jojo

--- server/parsing.js
+++ server/parsing.js
@@ -6,9 +6,9 @@ var urlapi = Meteor.npmRequire('url');
 var Twit = Meteor.npmRequire('twit');
 
 var T = new Twit({
-	consumer_key: Meteor.settings.private.twitter.consumer_key, // API key
+	consumer_key: Meteor.settings.public.twitter.consumer_key, // API key
 	consumer_secret: Meteor.settings.private.twitter.consumer_key_secret, // API secret
-	access_token: Meteor.settings.private.twitter.access_token,
+	access_token: Meteor.settings.public.twitter.access_token,
 	access_token_secret: Meteor.settings.private.twitter.access_token_secret
 });
 
