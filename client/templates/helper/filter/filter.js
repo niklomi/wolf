cad24965a6c4c02e00376a48ab2148e2e5947041
niklomi <nikloomi@gmail.com>
2016-01-06 01:20:58 +0300

    yol

--- client/templates/helper/filter/filter.js
+++ client/templates/helper/filter/filter.js
@@ -1,5 +1,6 @@
 Template.filter.onCreated(function(){
 	this.select = new ReactiveVar(false);
+	this.search_tag_click = new ReactiveVar(false);
 	Session.set('find-tags',[]);
 });
 
@@ -19,6 +20,9 @@ Template.filter.helpers({
 	tags:function(){
 		let template = Template.instance();
 		if (Session.get('all_tags') && Session.get('all_tags').length > 0) return Session.get('all_tags');
+	},
+	search_tag_click:function(){
+		return Template.instance().search_tag_click.get();
 	}
 })
 
@@ -27,11 +31,21 @@ Template.filter.events({
 		template.select.get() === true ? template.select.set(false) : template.select.set(true);
 	},
 	'click .filter-head-input':function(e,template){
+		console.log(!template.search_tag_click.get());
+		template.search_tag_click.set(!template.search_tag_click.get());
 		if (Session.get('all_tags')) return false;
 		get_tags_array();
 	},
 	'click .chose-tag':function(event,template){
 		let tag = $(event.currentTarget).text().trim().toLowerCase();
 		find_add_tag(tag);
+	},
+	'keyup #live-search':function(event){
+		let value = event.target.value;
+		if (value.trim().length > 0 && Session.get('countofshow') !== 2000){
+			Session.set('countofshow', 2000);
+		} else if (value.trim().length === 0){
+			Session.set('countofshow', 50);
+		}
 	}
 })
--- client/templates/layout.js
+++ client/templates/layout.js
@@ -1,6 +1,9 @@
 Template.main_page.onRendered(function(){
+	$(window).resize(function(){
+		$('#future-fix').css('width',$('.house-right').css('width'));
+	});
 	$(window).scroll(function(){
-		$('#future-fix').css('max-width','267px');
+		$('#future-fix').css('width',$('.house-right').css('width'));
 		if ($(window).scrollTop() > $('.header').height() + 70) {
 			$('#future-fix').addClass('add-fix');
 		} else {
