1698b820ad6b6b37fa491a58bd1287cee2c47b53
niklomi <nikloomi@gmail.com>
2016-02-22 03:23:16 +0300

    fix twi tags

--- client/templates/contents/single-job/existJob.js
+++ client/templates/contents/single-job/existJob.js
@@ -23,7 +23,6 @@ Template.exist_job.onCreated(function(){
         seoImage = Meteor.absoluteUrl().slice(0, -1) + checkedImage;
       }else
         seoImage = Meteor.absoluteUrl() + 'image/twitter-image-262x262.png';
-      console.log(image, checkedImage, checkedImage !== image);
       setSEO(title, description, `http://remotewolfy.com${FlowRouter.current().path}`, seoImage);
     });
     self.ready.set(handle.ready());
